db1f7b7864405580a30d748ed573f59bfd15256d
katonori <katonori.d@gmail.com>
2014-07-20 22:32:02 +0900

    added a demangle switch.

--- search_symbol.py
+++ search_symbol.py
@@ -1,14 +1,26 @@
-import re
-import sys
-import commands
+import re, sys, commands, getopt
+
+def usage():
+    print "usage: cmd [-C] word file0 [file1 ...]"
 
 if len(sys.argv) <= 2:
-    print "usage: cmd word file0 [file1 ...]"
+    usage()
     sys.exit(1)
 
-word = sys.argv[1]
-for lib in sys.argv[2:]:
-    (rv, out) = commands.getstatusoutput("nm -C " + lib)
+nmOpt = []
+
+try:
+    opts, argv = getopt.getopt(sys.argv[1:], "C", [])
+except getopt.GetoptError:
+    usage()
+    sys.exit(2)
+for o, a in opts:
+    if o == "-C":
+        nmOpt.append(o)
+
+word = argv[0]
+for lib in argv[1:]:
+    (rv, out) = commands.getstatusoutput("nm " + " ".join(nmOpt) + " " + lib)
     objName = ""
     for l in out.split("\n"):
         m = re.match(r"^(\S+):", l)
